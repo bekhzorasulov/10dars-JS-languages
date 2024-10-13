23c00b94642ee243ab86bf332bc6584843f20363
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-10-14 00:24:43 +0500

    top5languages

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 import country from "./country.js";
 
-// function getTopLanguages(country) {
-//   const languageCount = {};
-//   country.forEach((country) => {
-//     country.languages.forEach((language) => {
-//       if (languageCount[language]) {
-//         languageCount[language]++;
-//       } else {
-//         languageCount[language] = 1;
-//       }
-//     });
-//   });
-//   const sortedLanguages = Object.entries(languageCount).sort(
-//     (a, b) => b[1] - a[1]
-//   );
-//   const top5Languages = sortedLanguages.slice(0, 5);
-//   return top5Languages;
-// }
-// console.log(getTopLanguages(country));
-
 const getToplanguages = country.reduce((acc, curVal) => {
   if (curVal.languages) {
     const getLanguages = Object.values(curVal.languages);
@@ -32,8 +13,7 @@ const getToplanguages = country.reduce((acc, curVal) => {
   }
   return acc;
 }, {});
-console.log(getToplanguages);
-
+// console.log(getToplanguages);
 const sortedLanguages = Object.entries(getToplanguages).sort(
   (a, b) => b[1] - a[1]
 );
